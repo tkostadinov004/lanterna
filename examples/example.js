fe75c0bfa84dd2d06b3f92cdb25549aece2f4ea6
tkostadinov004 <tkostadinov04@gmail.com>
2026-04-04 15:39:58 +0300

    naming error fix

--- examples/example.js
+++ examples/example.js
@@ -1,4 +1,4 @@
-const lanterna = require('../scripts/index.js'); // replace with 'lanterna'
+const lanterna = require('../src/index.js'); // replace with 'lanterna'
 
 const url = 'https://docs.google.com/presentation/d/e/2PACX-1vSEfzYEyByG0NgYkP6uNPMOBy5EUoT2f0bQReydEHTf7kHSN1qcFUY9Bs1NSM5ZQOf2pflTWjm1QFsY/pub';
 
